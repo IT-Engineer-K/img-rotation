6e0b7b65a15632f9c57ae1058738e834547e80b9
IT-Engineer-K <76894075+IT-Engineer-K@users.noreply.github.com>
2021-09-20 23:26:52 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -3,16 +3,12 @@ var deg = 0;
 window.addEventListener("deviceorientation", handleOrientation, true);
 
 function handleOrientation(event) {
-    img.style.transformStyle = "preserve-3d";
     var alpha = event.alpha;
-    img.style.transform = `rotateX(${alpha}deg)`;
-    var beta = event.beta;
-    img.style.transform = `rotateY(${beta}deg)`;
-    var gamma = event.gamma;
-    img.style.transform = `rotateZ(${gamma}deg)`;
-    img.style.transform = `rotate3d(${alpha/360}, ${beta/360}, ${gamma/360}, 0deg)`;
+    img.style.transform = `rotate(${alpha}deg)`;
 }
 const update = () => {
     deg++;
+    if (deg == 360)
+        deg = 0
 }
 setInterval(update, 10)
