ca60a37cc204f9f0758b90517551ea134f21f203
IT-Engineer-K <76894075+IT-Engineer-K@users.noreply.github.com>
2021-09-20 22:33:20 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ window.addEventListener("deviceorientation", handleOrientation, true);
 
 function handleOrientation(event) {
     var alpha = event.alpha;
-    img.style.transform = `rotate(${alpha}deg)`;
+    img.style.transform = `rotateX(${alpha}deg)`;
+    var beta = event.beta;
+    img.style.transform = `rotateY(${beta}deg)`;
+    var gamma = event.gamma;
+    img.style.transform = `rotateZ(${gamma}deg)`;
 }
 const update = () => {
 
